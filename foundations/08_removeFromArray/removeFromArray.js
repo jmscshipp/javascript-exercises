c76298c35af989c7762db114f99275a60af4ce6e
beebop <jmscshipp202@gmail.com>
2026-02-17 17:02:52 -0600

    finished 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...TheArgs) 
+{
+    return array.filter((val) => !TheArgs.includes(val));
 };
+
+const array = [1, 2, 3, 4, 5];
+console.log(removeFromArray(array, 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
